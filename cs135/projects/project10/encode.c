ac88b0774f1c6dc9eb8155117685d210c44cc62a
Justin Stevens <j6penguin@gmail.com>
2016-05-01 19:22:14 -0700

    FUCK YES

--- cs135/projects/project10/encode.c
+++ cs135/projects/project10/encode.c
@@ -4,24 +4,44 @@ Purpose: Encodes a message from the user into a PGM file
 Author:  Justin Stevens */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
 int main(int argc, char *argv[]){
+  if(argc!=3){
+    printf("Failure:  Wrong number of files");
+    exit(EXIT_FAILURE);
+  }
   FILE *original=fopen(argv[1], "r");
-  // FILE *message=fopen(argv[2], "r");
+  if(original==NULL){
+    printf("Failure:  Input image did not open properly");
+    exit(EXIT_FAILURE);
+  }
   FILE *output=fopen(argv[2], "w");
-  char *image_header;
+  if(output==NULL){
+    printf("Failure:  Output image was not written to successfully");
+    exit(EXIT_FAILURE);
+  }
+
   char image_character;
-  while (image_character=fgetc(original)!='\n'){
+  while((image_character=fgetc(original))!='\n'){
+    fprintf(output, "%c", image_character);
   }
-  while (image_character=fgetc(original)!='\n'){
+  fprintf(output, "\n");
+  while((image_character=fgetc(original))!='\n'){
+    fprintf(output, "%c", image_character);
   }
+  fprintf(output, "\n");
   int row, col;
-  fscanf(original, "%d %d", row, col);
-  printf("%d %d", row, col);
+  fscanf(original, "%d %d", &row, &col);
+
+  unsigned char *image;
+  printf("%d", sizeof(unsigned char)*row*col);
+  image=(unsigned char *) malloc(sizeof(unsigned char)*row*col);
 
   fclose(original);
-  fclose(output);  
+  fclose(output);
   // unsigned char image=malloc()
+  return 0;
 }
