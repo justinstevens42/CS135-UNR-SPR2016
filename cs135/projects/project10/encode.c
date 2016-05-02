88fa3d2ea584beec6ae5755e5a2543583c070240
Justin Stevens <j6penguin@gmail.com>
2016-05-01 19:00:58 -0700

    fml

--- cs135/projects/project10/encode.c
+++ cs135/projects/project10/encode.c
@@ -10,15 +10,18 @@ Author:  Justin Stevens */
 int main(int argc, char *argv[]){
   FILE *original=fopen(argv[1], "r");
   // FILE *message=fopen(argv[2], "r");
-  FILE *write=fopen(argv[2], "w");
+  FILE *output=fopen(argv[2], "w");
   char *image_header;
   char image_character;
-  while (image_character=getc(original)!='\n'){
-    printf("%c", image_character);
-    // fputc(image_character, write);
+  while (image_character=fgetc(original)!='\n'){
   }
-  while (image_character=getc(original)!='\n'){
-    // fputc(image_character, write);
+  while (image_character=fgetc(original)!='\n'){
   }
+  int row, col;
+  fscanf(original, "%d %d", row, col);
+  printf("%d %d", row, col);
+
+  fclose(original);
+  fclose(output);  
   // unsigned char image=malloc()
 }
